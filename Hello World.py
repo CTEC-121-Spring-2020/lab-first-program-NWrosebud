072b0d4a1c540bbba643fde27f0dac6faa3fb514
NWrosebud <heatherofthedawn@gmail.com>
2020-04-23 14:45:47 -0700

    Completed program for first lab

---
+++ Hello World.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+Heather Barnett
+First Lab
+Hello World
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Hello World!")
+    print()
+    print()
+
+main()    
